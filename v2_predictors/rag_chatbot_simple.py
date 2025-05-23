2d7b194fdf5cafe07a97dc0472d9d0502842e0ee
Joni Kerkelä <jonikerkela@hotmail.com>
2025-05-23 13:44:22 +0300

    Add retrieval correction analysis step to RAG pipeline

--- v2_predictors/rag_chatbot_simple.py
+++ v2_predictors/rag_chatbot_simple.py
@@ -31,7 +31,16 @@ from langchain_core.prompts import PromptTemplate
 from typing_extensions import List, TypedDict, Annotated
 from typing import Literal
 
-WEB_SOURCE = "https://lilianweng.github.io/posts/2023-06-23-agent/"
+parser = argparse.ArgumentParser("generic_analysis")
+parser.add_argument('--langsmith_api_key', required=True)
+parser.add_argument('--huggingface_api_key', required=True)
+parser.add_argument('--use_existing_storage', dest='use_existing_storage', default=False, action=argparse.BooleanOptionalAction)
+parser.add_argument('--append_to_storage', dest='append_to_existing_storage',
+    help='data source is appended to existing storage if used, if not data source will be stored to in memory storage only and used from there', default=False, action=argparse.BooleanOptionalAction)
+parser.add_argument('--query', required=True)
+parser.add_argument('--sources', nargs='+', required=False,
+    help='The sources to use to answer for query, can be different types of documents or web page, if used with --append_to_storage, the sources will be stored to persistent storage')
+args = parser.parse_args()
 
 os.environ["LANGSMITH_TRACING"] = "true"
 
@@ -48,41 +57,45 @@ class FileTypeSupport(Enum):
 #TODO: the answer is too long and it contains extra information not directly related to question.
 RAG_PROMPT = """
 Instructions: \
-Use the provided context to answer the question at the end. \
-Treat the whole context as equal, do not prioritize any section, even if question contains section information. \
-If you don't find the answer from the context, just say that you don't know. \
-Always immdiately stop after you have provided first answer, do not provide follow-up questions or answers.
+Use the provided context to answer the question at the end. 
+Treat the whole context as equal, do not prioritize any section, even if question contains section information. 
+If you don't find the answer from the context, just say that you don't know. 
+Always immdiately stop after you have provided first answer, do not provide follow-up questions or answers.\n
 
-Context: {context}
+Context: {context}\n
 
 Question: {question}
 """
 
 QUERY_ANALYSIS_PROMPT = """
-Generate a JSON object for the following question:
+Generate a JSON object for the following question:\n
 {question}
 
-Return valid JSON containing query and section, in format:
-{{
-    "section": "<one of 'beginning', 'middle', 'end', undefined>",
-    "query": "<question>"
-}}
+Shorten the question to consise format removing any extra words. Return valid JSON containing question and section, in format:\n
+{{\n
+    "section": "<one of 'beginning', 'middle', 'end', undefined>",\n
+    "query": "<question>"\n
+}}\n
 
-json:"""
+json:
+"""
+
+RETRIEVAL_ANALYSIS_CORRECTION_PROMPT= """
+Check if the document contains keyword(s) or semantic meaning related to the question
+Give answer in json format as:\n
+{{\n
+    "{key_name}": "<one of 'YES', 'NO'">\n
+}}\n
+
+Document: {document}\n
+
+Question: {question}\n\
+
+json:
+"""
 
 VECTOR_STORAGE = "faiss_index_RAG"
 BM25_STORAGE = "bm25_index.pkl"
-
-parser = argparse.ArgumentParser("stock_predict")
-parser.add_argument('--langsmith_api_key', required=True)
-parser.add_argument('--huggingface_api_key', required=True)
-parser.add_argument('--use_existing_storage', dest='use_existing_storage', default=False, action=argparse.BooleanOptionalAction)
-parser.add_argument('--append_to_storage', dest='append_to_existing_storage',
-    help='data source is appended to existing storage if used, if not data source will be stored to in memory storage only and used from there', default=False, action=argparse.BooleanOptionalAction)
-parser.add_argument('--query', required=True)
-parser.add_argument('--sources', nargs='+', required=False,
-    help='The sources to use to answer for query, can be different types of documents or web page, if used with --append_to_storage, the sources will be stored to persistent storage')
-args = parser.parse_args()
 
 class SearchQuery(TypedDict):
     query: Annotated[str, ..., "Search query to run."]
@@ -118,28 +131,22 @@ def getFileTypeSupport(file_path):
         return FileTypeSupport.FITZ
     else:
         return FileTypeSupport.NON_SUPPORTED
-        
-def retrieveContextFromStore(state, reranker_llm, vector_store, bm25_index_data):
-    top_k = 10
-    top_k_rerank = 5
-    query = state["query"]
-    retrieved_docs = vector_store.similarity_search(query["query"], k=top_k)
-    retrieved_doc_ids = []
+
+def getRelevantDocsFromVectorStore(query, vector_store, top_documents_limit):
+    print(f"DEBUG: Getting document matches from vector store")
+    retrieved_docs = vector_store.similarity_search(query["query"], k=top_documents_limit)
     if not query["section"] == QUERY_SPLIT_UNDEFINED_LABEL:
         filtered_docs = [doc for doc in retrieved_docs if doc.metadata.get("section") == query["section"]]
         if filtered_docs:
             retrieved_docs = filtered_docs
-    
-    for doc in retrieved_docs:
-        doc_id = doc.metadata.get("id")
-        if doc_id is not None:
-            retrieved_doc_ids.append(doc_id)
-    
-    
+    return retrieved_docs 
+
+def getBM25RankedDocs(query, bm25_index_data, top_documents_limit, already_retrieved_doc_ids):
+    print(f"DEBUG: Getting document matches from ranking store")
     bm25_engine = bm25_index_data["bm_engine"]
     original_docs_data = bm25_index_data["documents"]
     query_tokens = bm25s.tokenize(query["query"], stopwords="en")
-    docs_as_text, bm25_scores = bm25_engine.retrieve(query_tokens, k=top_k)
+    docs_as_text, bm25_scores = bm25_engine.retrieve(query_tokens, k=top_documents_limit)
     bm25_scores = bm25_scores[0]
     number_of_first_chars_to_compare = 20
 
@@ -157,7 +164,7 @@ def retrieveContextFromStore(state, reranker_llm, vector_store, bm25_index_data)
             continue
         score = bm25_scores[idx]
         # Skip if document is already in the vector store retrieval results
-        if original_index in retrieved_doc_ids:
+        if original_index in already_retrieved_doc_ids:
             continue
         bm25_results.append({
             "id": original_index,
@@ -165,19 +172,62 @@ def retrieveContextFromStore(state, reranker_llm, vector_store, bm25_index_data)
             "score": score,
             "doc": orig_doc_full,
         })
-        
-    combined_docs = []
-    if bm25_results:
-        combined_docs += [item["doc"] for item in bm25_results]
-    if retrieved_docs:
-        combined_docs += retrieved_docs
+    return bm25_results
     
-    doc_texts = [doc.page_content for doc in combined_docs]
-    doc_rank_infos = reranker_llm.rank(query["query"], doc_texts, top_k=top_k_rerank)
+def getRerankedDocs(reranker_llm, query, docs, top_documents_limit):
+    print(f"DEBUG: Reranking documents with LLM")
+    doc_texts = [doc.page_content for doc in docs]
+    doc_rank_infos = reranker_llm.rank(query["query"], doc_texts, top_k=top_documents_limit)
     reranked_docs = []
     for doc_rank_info in doc_rank_infos:
-        reranked_docs.append(combined_docs[doc_rank_info["corpus_id"]])
-    return {"context": reranked_docs}
+        reranked_docs.append(docs[doc_rank_info["corpus_id"]])
+    return reranked_docs
+    
+def getCorrectionCheckedDocs(correction_check_llm, query, docs_after_re_ranking):
+    print(f"DEBUG: Filtering relevant documents with LLM")
+    rag_prompt = PromptTemplate.from_template(RETRIEVAL_ANALYSIS_CORRECTION_PROMPT)
+    correction_passed_docs = []
+    for doc in docs_after_re_ranking:
+        messages = rag_prompt.invoke({"question": query["query"], "document": doc.page_content})
+        response = correction_check_llm(messages.to_string(), max_new_tokens=24, do_sample=False, top_p=None, temperature=None)
+        print(f'Relevance check result: {response}')
+        if isinstance(response, list) and len(response) > 0:
+            print(f"DEBUG: Check query analysis answer: {response}")
+            response_text = response[0].get("generated_text", "")
+            try:
+                response_val = json.loads(response_text)[RELEVANT_ARTICALE_JSON_KEY_NAME]
+                if response_val == "YES":
+                    correction_passed_docs.append(doc)
+            except json.JSONDecodeError as e:
+                print(f"Error parsing the response with error: {e}")
+        else:
+            print("Warning: Unexpected response format from LLM.")
+            
+    return correction_passed_docs
+    
+    
+def retrieveContextFromStore(state, reranker_llm, correction_check_llm, vector_store, bm25_index_data):
+    top_k = 10
+    top_k_rerank = 5
+    query = state["query"]
+    retrieved_docs = getRelevantDocsFromVectorStore(query, vector_store, top_k)
+    retrieved_doc_ids = []
+    for doc in retrieved_docs:
+        doc_id = doc.metadata.get("id")
+        if doc_id is not None:
+            retrieved_doc_ids.append(doc_id)
+    
+    retrieved_ranker_docs = getBM25RankedDocs(query, bm25_index_data, top_k, retrieved_doc_ids)
+    
+        
+    combined_docs = []
+    if retrieved_ranker_docs:
+        combined_docs += [item["doc"] for item in retrieved_ranker_docs]
+    if retrieved_docs:
+        combined_docs += retrieved_docs
+    docs_after_re_ranking = getRerankedDocs(reranker_llm, query, combined_docs, top_k_rerank)
+    final_docs_after_source_correction_check = getCorrectionCheckedDocs(correction_check_llm, query, docs_after_re_ranking)
+    return {"context": final_docs_after_source_correction_check}
 
 def analyzeQuery(state, llm):
     query = {"query": state['question'], "section": QUERY_SPLIT_UNDEFINED_LABEL}
@@ -212,14 +262,12 @@ def generateResponse(state, llm):
     
 def loadDataSourceFromWeb(web_page):
     loader = WebBaseLoader(
-        web_paths=(web_page,),
-        bs_kwargs=dict(
-            parse_only=bs4.SoupStrainer(
-                class_=("post-content", "post-title", "post-header")
-            )
+        web_paths=(web_page,)
         ),
     )
-    return loader.load()
+    unfiltered_page = loader.load()
+    readable_doc = ReadableDocument(unfiltered_page[0].page_content)
+    return Document(readable_doc.summary())
    
 def updateRankingStorage(source_splits, store_persistently):
     documents_as_text = [doc.page_content for doc in source_splits]
@@ -233,12 +281,13 @@ def updateRankingStorage(source_splits, store_persistently):
     return bm25_index_data
 
 def main():
-    print(f"DEBUG: starting to load depedency llm models")
+    print(f"DEBUG: starting to load dependency llm models")
     if not os.environ.get("LANGSMITH_API_KEY"):
         os.environ["LANGSMITH_API_KEY"] = args.langsmith_api_key
         
     login(token=args.huggingface_api_key)
     
+    #TODO: replace with more powerful LLM for accuracy e.g. meta-llama/Llama-3.2-3B-Instruct
     model_name = "meta-llama/Llama-3.2-1B"
     llm = pipeline("text-generation",
         model=model_name, model_kwargs={"torch_dtype": "bfloat16"},
@@ -314,7 +363,7 @@ def main():
     print(f"DEBUG: running the LLM with augmented context for query: {args.query}")
     graph_builder = StateGraph(LLMState).add_sequence([
         ("analyzeQuery", partial(analyzeQuery, llm=llm)),
-        ("retrieveContext", partial(retrieveContextFromStore, reranker_llm=reranker_llm, vector_store=vector_store, bm25_index_data=bm25_index_data)),
+        ("retrieveContext", partial(retrieveContextFromStore, reranker_llm=reranker_llm, correction_check_llm=llm, vector_store=vector_store, bm25_index_data=bm25_index_data)),
         ("generateResponse", partial(generateResponse, llm=llm))
     ])
     graph_builder.add_edge(START, "analyzeQuery")
